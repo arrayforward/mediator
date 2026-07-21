3dbf85f88121efeb65bd87b054bfe14d64082438
arrayforward <arrayforward@users.noreply.github.com>
2026-07-21 18:20:45 +0800

    chore: drop accidentally committed build-wsl artifacts

--- build-wsl/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/vit/mediator/tests
-# Build directory: /mnt/d/vit/mediator/build-wsl/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/d/vit/mediator/build-wsl/tests/mediator_tests[1]_include.cmake")
